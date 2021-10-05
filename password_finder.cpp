003726c0be52c7577ccc6cf0287cf2586c2d8216
XCanL996 <86374458+XCanL996@users.noreply.github.com>
2021-10-05 19:37:06 -0400

    main function written to find the right password

--- password_finder.cpp
+++ password_finder.cpp
@@ -1,10 +1,42 @@
 #include<iostream>
 #include<string>
-#include <fstream>
+#include<fstream>
+#include<vector>
 
 using namespace std;
 
 int main(){
+
+  vector<string>myVector;
+  string line;
+  string inputtedLine;
+  string myPassword;
+  size_t found;
+
+  int i = 0;
+
+  ifstream myfile;
+  myfile.open("Words.txt");
+  if(myfile.is_open()){
+    while ( getline (myfile,line) )
+      {
+        myVector.insert(myVector.begin() + i,line);
+        //cout << line;
+        i++;
+      }
+    myfile.close();
+  }
+  else cout << "Unable to open file";
+
+  cout << "Please enter a number to find your password" << endl;
+  cin >> inputtedLine;
+
+  for (int i=0; i < myVector.size() ;i++) {
+    found  = myVector[i].find(inputtedLine);
+      if (found  == 0){
+        cout << myVector[i] << '\n';
+      }
+    }
 
   return 0;
 }
